448cf7e96fec68a1b96233026f8f051b42b83e89
Grace <gracebolutife@gmail.com>
2021-07-03 12:26:41 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 "# SureHealth" 
+This project is an hospital booking system.
+It is built with Springboot framework and is a mutlimodule project.
+Within it, you will find the data, service and web module.
+Database used is MySQL. 
+To use this project, clone from github.
+Install any IDE for java.
+Set up MySQL database. You can see your database properties in the application.properfile file in the parent module.
+when database is properly set up, the project will run successfully.
+There are four APIs. 
+user/add-user this throws an exception when an exisiting email is being added
+user/remove-user
+user/make-appointment
+user/remove-appointment
+
+You can test the APIs on postman.
--- web/src/main/java/com/hospital/apis/UserController.java
+++ web/src/main/java/com/hospital/apis/UserController.java
@@ -1,6 +1,8 @@
 package com.hospital.apis;
 
 import com.hospital.dtos.AddUserDto;
+import com.hospital.dtos.AppointmentRequest;
+import com.hospital.dtos.RemoveAppointmentRequest;
 import com.hospital.dtos.RemoveUserRequest;
 import com.hospital.exceptions.AppException;
 import com.hospital.services.UserService;
@@ -20,13 +22,27 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/add-user")
-    public ResponseEntity<?> registerDoctor(@RequestBody AddUserDto request) throws AppException{
+    public ResponseEntity<?> addUser(@RequestBody AddUserDto request) throws AppException{
         var response = userService.addUsers(request);
         return ResponseEntity.ok(response);
     }
 
+    @PostMapping("/remove-user")
     public ResponseEntity<?> removeUser(@RequestBody RemoveUserRequest request){
         var response = userService.removeUser(request);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/make-appointment")
+    public ResponseEntity<?> makeAppointment(@RequestBody AppointmentRequest request) throws AppException{
+        var response = userService.makeAppointment(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/remove-appointment")
+    public ResponseEntity<?> removeUser(@RequestBody RemoveAppointmentRequest request) throws AppException {
+        var response = userService.removeAppointment(request);
+        return ResponseEntity.ok(response);
+    }
+
 }
